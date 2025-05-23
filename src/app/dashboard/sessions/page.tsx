cec6e3e9d8db1e2cf78e89e28d05f3dfad104be6
python-fuse <hauwamuktar880@gmail.com>
2025-05-23 20:47:13 +0100

    feat: add Skeleton component for loading state in sessions page

--- src/app/dashboard/sessions/page.tsx
+++ src/app/dashboard/sessions/page.tsx
@@ -13,6 +13,7 @@ import { capitalize, toast } from "@/lib/utils";
 import DB from "@/supabase/db";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const page = () => {
   const [sessions, setSessions] = useState<DebugSession[]>([]);
@@ -39,30 +40,36 @@ const page = () => {
     }
   }, [user]);
 
-  const sessionsList = sessions.map((session) => {
+  if (loading) {
     return (
-      <Card
-        className="rounded-sm hover:shadow-lg duration-300"
-        key={session.id}
-      >
-        <CardContent>
-          <CardHeader>
-            <Link
-              href={`/dashboard/sessions/${session.id}`}
-              className="hover:text-blue-500 duration-300"
-            >
-              <CardTitle className="text-xl">
-                {capitalize(session.title || "")}
-              </CardTitle>
-            </Link>
-            <CardDescription>
-              {new Date(session.created_at || "").toLocaleString()}
-            </CardDescription>
-          </CardHeader>
-        </CardContent>
-      </Card>
+      <div className="flex flex-col space-y-4 p-4">
+        <h2 className="text-3xl font-bold text-gray-800">Sessions</h2>
+        <Skeleton className="h-24 w-full" />
+        <Skeleton className="h-24 w-full" />
+        <Skeleton className="h-24 w-full" />
+      </div>
     );
-  });
+  }
+
+  const sessionsList = sessions.map((session) => (
+    <Card className="rounded-sm hover:shadow-lg duration-300" key={session.id}>
+      <CardContent>
+        <CardHeader>
+          <Link
+            href={`/dashboard/sessions/${session.id}`}
+            className="hover:text-blue-500 duration-300"
+          >
+            <CardTitle className="text-xl">
+              {capitalize(session.title || "")}
+            </CardTitle>
+          </Link>
+          <CardDescription>
+            {new Date(session.created_at || "").toLocaleString()}
+          </CardDescription>
+        </CardHeader>
+      </CardContent>
+    </Card>
+  ));
 
   return (
     <div className="flex flex-col space-y-4 p-4">
